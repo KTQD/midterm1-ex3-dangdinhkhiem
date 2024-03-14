68fe06954201ae12750fdb2dd77620ffb9f9ec06
dangdinhkhiem <157661911+dangdinhkhiem@users.noreply.github.com>
2024-03-14 08:16:31 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,22 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-        // Đường dẫn của file input và output
+
         String inputFilePath = "input.txt";
         String outputFilePath = "output.txt";
 
         try {
-            // Đọc file input.txt
+
             FileReader fileReader = new FileReader(inputFilePath);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
 
-            // Tạo và chuẩn bị file output.txt
+
             FileWriter fileWriter = new FileWriter(outputFilePath);
             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
 
             String line;
 
-            // Đọc từng dòng trong file input.txt
+
             while ((line = bufferedReader.readLine()) != null) {
                 // Thay thế từ "Nha Trang" thành "Vũng Tàu" và ghi vào file output.txt
                 line = line.replaceAll("Nha Trang", "Vũng Tàu");
@@ -25,7 +25,7 @@ public class Main {
                 bufferedWriter.newLine();
             }
 
-            // Đóng file input và output
+            
             bufferedReader.close();
             bufferedWriter.close();
 
